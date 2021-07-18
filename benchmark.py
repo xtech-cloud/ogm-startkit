151f4d44b8ab93cda26d2ab8b088297e898eb9f7
Easlee Liu <easlee@outlook.com>
2021-07-18 17:37:36 +0000

    upgrade benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -10,7 +10,7 @@ from threading import Thread, Lock
 BEGIN
 '''
 
-domain = 'http://127.0.0.1:18800'
+domain = 'http://localhost:8080'
 
 urls = [
         domain+'/ogm/startkit/Healthy/Echo',
@@ -24,8 +24,8 @@ options = {
         'content-type':'application/json;charset=UTF-8'
         }
 
-thread_count = 100
-invoke_count = 10
+thread_count = 200
+invoke_count = 20
 interval = 0
 
 
@@ -40,25 +40,24 @@ failed = 0
 successed = 0
 thread_finished = 0
 errors = {}
+cps = 0 #call per seconds
+cps_successed = 0
+cps_failed = 0
+exit = False
 def onThreadFinish(_id):
     global thread_finished
     lock.acquire()
     thread_finished = thread_finished + 1
     lock.release()
-    if thread_finished == thread_count:
-        print('访问成功总次数：%d'%(successed))
-        print('访问失败总次数：%d'%(failed))
-        print('----------------------------------------------------------')
-        print('错误列表：')
-        for err in errors:
-            print(err)
-        print('##########################################################')
 
 def call(_id, _n):
     global failed
     global successed
+    global cps_failed
+    global cps_successed
     global interval
     global errors
+    global cps
     for i in range(0, _n):
         time.sleep(interval)
         data = json.dumps(params)
@@ -66,18 +65,48 @@ def call(_id, _n):
         url = urls[random.randint(0, len(urls)-1)]
         request = urllib.request.Request(url, data, options)
         try:
-            reply = urllib.request.urlopen(request).read().decode('utf-8')
+            req = urllib.request.urlopen(request)
+            reply = req.read().decode('utf-8')
+            req.close()
+            del req
             #print(reply)
             lock.acquire()
             successed = successed + 1
+            cps_successed = cps_successed + 1
             lock.release()
         except Exception as e:
             err = str.format('%s -> %s'%(url,e))
             lock.acquire()
             failed = failed + 1
+            cps_failed = cps_failed + 1
             errors[err] = 0
             lock.release()
+        lock.acquire()
+        cps = cps + 1
+        lock.release()
     onThreadFinish(_id)
+
+def tick_print():
+    global cps
+    global cps_successed
+    global cps_failed
+    tick = 0
+    while True:
+        time.sleep(1)
+        tick = tick + 1
+        print('> 【每秒】访问次数：%d, 成功：%d，失败：%d, 【总计】耗时：%d秒，成功：%d，失败：%d，完成线程：%d，剩余任务：%d'%(cps, cps_successed, cps_failed, tick, successed, failed, thread_finished, thread_count*invoke_count - failed-successed))
+        lock.acquire()
+        cps = 0
+        cps_successed = 0
+        cps_failed = 0
+        lock.release()
+        if thread_finished == thread_count:
+            break
+    print('----------------------------------------------------------')
+    print('错误列表：')
+    for err in errors:
+        print(err)
+    print('##########################################################')
 
 print('##########################################################')
 print('接口列表：')
@@ -88,6 +117,8 @@ print('线程数：%d '%(thread_count))
 print('每线程访问次数：%d '%(invoke_count))
 print('访问间隔时间：%f '%(interval))
 print('**********************************************************')
+pt = Thread(target=tick_print)
+pt.start()
 # 创建100个线程
 for i in range(0,thread_count):
     lock = Lock()
